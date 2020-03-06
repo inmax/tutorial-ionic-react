223ee3ae3e07e5b5802bfcbeed47afabf732d159
inmax <inmaculadaminarroruiz@gmail.com>
2020-03-06 12:50:55 +0100

    Integrar useFilesystem y useStorage

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -7,29 +7,33 @@ import { isPlatform } from '@ionic/react';
 import { CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory } from "@capacitor/core";
 
 const PHOTO_STORAGE = "photos";
-// const { get, set } = useStorage();
-
 
 export function usePhotoGallery() {
+  //https://capacitor.ionicframework.com/docs/apis/camera/
+    const { getPhoto } = useCamera(); //react-hook , llama a la api de capacitor 
+    const { get, set } = useStorage(); //
     const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
-    const [photos, setPhotos] = useState<Photo[]>([]);
-    const { getPhoto } = useCamera();
+    
+    //Usamos UseState del API de react para almacenar en el array "photos", cada foto sacada.
+    const [photos, setPhotos] = useState<Photo[]>([]); //Interface <Photo
+    
+    //Usamos UseEffect del API de react para descargar las photos guardas cada vez que el componente camara se actualiza.
+    useEffect(() => {
+      const loadSaved = async () => {
+        const photosString = await get('photos');
+        const photos = (photosString ? JSON.parse(photosString) : []) as Photo[];
+        for (let photo of photos) {
+          const file = await readFile({
+            path: photo.filepath,
+            directory: FilesystemDirectory.Data
+          });
+          photo.base64 = `data:image/jpeg;base64,${file.data}`;
+        }
+        setPhotos(photos);
+      };
+      loadSaved();
+    }, [get, readFile]);
 
-    // useEffect(() => {
-    //     const loadSaved = async () => {
-    //       const photosString = await get('photos');
-    //       const photos = (photosString ? JSON.parse(photosString) : []) as Photo[];
-    //       for (let photo of photos) {
-    //         const file = await readFile({
-    //           path: photo.filepath,
-    //           directory: FilesystemDirectory.Data
-    //         });
-    //         photo.base64 = `data:image/jpeg;base64,${file.data}`;
-    //       }
-    //       setPhotos(photos);
-    //     };
-    //     loadSaved();
-    //   }, [get, readFile]);
 
     const takePhoto = async () => {
         const cameraPhoto = await getPhoto({
@@ -37,30 +41,20 @@ export function usePhotoGallery() {
           source: CameraSource.Camera,
           quality: 100
         });
-      
         const fileName = new Date().getTime() + '.jpeg';
         const savedFileImage = await savePicture(cameraPhoto, fileName);
         const newPhotos = [savedFileImage, ...photos];
         setPhotos(newPhotos);
 
-        // set(PHOTO_STORAGE, JSON.stringify(newPhotos.map(p => {
-        //     // Don't save the base64 representation of the photo data, 
-        //     // since it's already saved on the Filesystem
-        //     const photoCopy = { ...p };
-        //     delete photoCopy.base64;
-        //     return photoCopy;
-        //   })));
+        set(PHOTO_STORAGE, JSON.stringify(newPhotos.map(p => {
+          // Don't save the base64 representation of the photo data, 
+          // since it's already saved on the Filesystem
+          const photoCopy = { ...p };
+          delete photoCopy.base64;
+          return photoCopy;
+        })));
+        console.log("PHOTO_STORAGE",PHOTO_STORAGE);
 
-
-          
-      };
-
-
-      const getPhotoFile = async (cameraPhoto: CameraPhoto, fileName: string): Promise<Photo> => {
-        return {
-          filepath: fileName,
-          webviewPath: cameraPhoto.webPath
-        };
       };
 
       const savePicture = async (photo: CameraPhoto, fileName: string) => {
@@ -71,6 +65,13 @@ export function usePhotoGallery() {
           directory: FilesystemDirectory.Data
         });
         return getPhotoFile(photo, fileName);
+      };
+      
+      const getPhotoFile = async (cameraPhoto: CameraPhoto, fileName: string): Promise<Photo> => {
+        return {
+          filepath: fileName,
+          webviewPath: cameraPhoto.webPath
+        };
       };
       
       return {
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -22,7 +22,7 @@ const Tab2: React.FC = () => {
           <IonRow>
             {photos.map((photo, index) => (
               <IonCol size="6" key={index}>
-                <IonImg src={photo.webviewPath} />
+              <IonImg src={photo.base64 ?? photo.webviewPath} />
               </IonCol>
             ))}
           </IonRow>
